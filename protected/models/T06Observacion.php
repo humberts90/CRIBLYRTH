7409295f8500ba46a1a41b40a3cc2db8d938b217
maverick710 <samuel.useche@unet.edu>
2014-01-27 22:58:03 -0430

    Parte de los jurados en un 40%

--- protected/models/T06Observacion.php
+++ protected/models/T06Observacion.php
@@ -17,6 +17,7 @@ class T06Observacion extends CActiveRecord
 	/**
 	 * @return string the associated database table name
 	 */
+	public $j1,$j2,$j3;
 	public function tableName()
 	{
 		return 't06_observacion';
--- protected/views/comision/menu.php
+++ protected/views/comision/menu.php
@@ -1,6 +1,7 @@
 <?php
 
 $this->menu=array(
+	array('label'=>'Inicio', 'url'=>array('index')),
 	array('label'=>'Modulo de actas', 'url'=>array('acta')),
 	array('label'=>'Modulo de noticias', 'url'=>array('noti')),
 	array('label'=>'Evaluar Anteproyectos', 'url'=>array('tesis')),
