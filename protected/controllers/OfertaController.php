b7e482f0cfd8c78d15e0f0861f6c3ee9e828c84e
maverick710 <samuel.useche@unet.edu>
2014-05-21 15:17:21 -0430

    ofertas al 80%

--- protected/controllers/OfertaController.php
+++ protected/controllers/OfertaController.php
@@ -27,15 +27,10 @@ class OfertaController extends Controller {
 	public function accessRules()
 	{
 		return array(
-<<<<<<< HEAD
-			
-			array('allow', 
-=======
-			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array('index','view'),
->>>>>>> ac105fb110649766139b543f27069193945cd6b9
+				
+			array('allow',  // deny all users
 				'users'=>array('*'),
-			)
+			),
 		);
 	}
 	public function actionIndex(){
@@ -43,30 +38,15 @@ class OfertaController extends Controller {
 	//	$listado = 
 		$this->render('index',array('Usuario'=>$tar,));
 	}
+
 	public function actionListar()
 	{
 		$tar=M05Usuario::model()->find("Usuario = '".Yii::app ()->user->name."'");	
 
 		$modelStatus = P03Status::model()->find("Descripcion = 'Oferta'");
 
-		$modelAsoc = T01TesisHasUsuario::model()->findAll("P03_id = '".$modelStatus->id."'");
-		$model_t = array();
-		$a=0;
-		foreach ($modelAsoc as $asoc) 
-		{
-			$model_t[$a] = M03Tesis::model()->find("id = '".$asoc->M03_id."'");
-			$a++;
-		}
-
-		$modelAsoc = T02PasantiaHasUsuario::model()->findAll("P03_id = '".$modelStatus->id."'");
-		$model_p = array();
-		$a=0;
-		foreach ($modelAsoc as $asoc) 
-		{
-			$model_p[$a] = M04Pasantia::model()->find("id = '".$asoc->M04_id."'");
-			$a++;
-		}
-
+		$model_t= M03Tesis::model()->findAll("P03_id = ".$modelStatus->id);
+		$model_p =M04Pasantia::model()->findAll("P03_id = ".$modelStatus->id);
 
 		$this->render('oferta',array('Usuario'=>$tar, 'tesis'=>$model_t, 'pasantias'=>$model_p));
 	}
