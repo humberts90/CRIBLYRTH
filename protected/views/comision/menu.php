dc0a013c7660035581ba317184d0c31c6ee1ce35
maverick710 <samuel.useche@unet.edu>
2014-06-19 15:42:53 -0430

    actualizando repositorio

--- protected/views/comision/menu.php
+++ protected/views/comision/menu.php
@@ -17,7 +17,7 @@ foreach ($roles as  $value) {
 	if(strcmp($value->p01->nombre,"Comisión del Tap")==0 && strcmp(Yii::app()->getController()->getId(),"jefe")!=0){
 		echo '</br>';
 		echo CHtml::link(CHtml::encode("Inicio ".$value->p01->nombre), array("comision/index"));
-	}
+	}///54545454g5er4g5e4
 
 	
 }
