cd59287ccae3f2d8aaa0b1e06629fabf9c8ff51f
katliss26 <katliss.26@gmail.com>
2014-06-28 16:49:14 -0430

    Acta de evaluacion de tesis, incluyendo si la tesis posee dos usuarios

--- protected/views/secretaria/_test.php
+++ protected/views/secretaria/_test.php
@@ -1,5 +1,4 @@
 
-	
 
 	<b><?php echo CHtml::encode($data->getAttributeLabel('Titulo')); ?>:</b>
 	<?php echo CHtml::encode($data->Titulo); ?>
@@ -8,6 +7,17 @@
 	<b><?php echo CHtml::encode('Fecha de la propuesta'); ?>:</b>
 	<?php echo CHtml::encode($data->Fecha_Inscripcion); ?>
 	<br />
+	<b><?php echo CHtml::encode('Fecha de la defensa'); ?>:</b>
+	<?php echo CHtml::encode($data->Fecha_Defensa); ?>
 	<br />
-	<?php echo CHtml::link(CHtml::encode("Ver detalles"), array('actaeva', 'id'=>$data->id)); ?>
+	<br />
+	<?php 
+	$TesisyAlumno = T01TesisHasUsuario::model()->findAll("M03_id=".$data->id." AND P02_id = '1'");
+	foreach ($TesisyAlumno as $value) {
+	echo "<br />";
+	echo CHtml::link(CHtml::encode("Descargar Planilla de evaluación de ".$value->m05->Nombre), array('actaevatesis', 'idEntrante'=>$value->M03_id.'-'.$value->M05_id )); 
+		
+	}
+
+	?>
 	<br />
