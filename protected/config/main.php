4f9d5fca687126cd7da21501684dbd243cc78333
katliss26 <katliss.26@gmail.com>
2014-06-28 11:01:22 -0430

    modulo de la secretaria

--- protected/config/main.php
+++ protected/config/main.php
@@ -83,7 +83,7 @@ return array(
 			'connectionString' => 'mysql:host=localhost;dbname=tapbd',
 			'emulatePrepare' => true,
 			'username' => 'root',
-			'password' => '',
+			'password' => 'root',
 			'charset' => 'utf8',
 		),
 	
