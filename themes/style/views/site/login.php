2bd9379b757a423be3d9cd467e466bfe66f059b3
maverick710 <maverick71036@gmail.com>
2014-07-24 15:31:48 -0430

    actualizando BD

--- themes/style/views/site/login.php
+++ themes/style/views/site/login.php
@@ -4,16 +4,14 @@
 /* @var $form CActiveForm  */
 
 $this->pageTitle=Yii::app()->name . ' - Login';
-$this->breadcrumbs=array(
-	'Login',
-);
 ?>
 
-<h1>Login</h1>
+<h1>Iniciar Sesion</h1>
 
-<p>Please fill out the following form with your login credentials:</p>
 
-<div class="form">
+<p>Por favor complete el siguiente formulario con sus datos de acceso:</p>
+
+<div class="form" >
 <?php $form=$this->beginWidget('CActiveForm', array(
 	'id'=>'login-form',
 	'enableClientValidation'=>true,
@@ -22,31 +20,25 @@ $this->breadcrumbs=array(
 	),
 )); ?>
 
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
+	<p class="note">Los campos con <span class="required">*</span> son requeridos.</p>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'username'); ?>
+		* Nombre de Usuario: </br>
 		<?php echo $form->textField($model,'username'); ?>
 		<?php echo $form->error($model,'username'); ?>
 	</div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'password'); ?>
+		* Contraseña: </br>
 		<?php echo $form->passwordField($model,'password'); ?>
 		<?php echo $form->error($model,'password'); ?>
-		<p class="hint">
-			Hint: You may login with <kbd>demo</kbd>/<kbd>demo</kbd> or <kbd>admin</kbd>/<kbd>admin</kbd>.
-		</p>
+	
 	</div>
 
-	<div class="row rememberMe">
-		<?php echo $form->checkBox($model,'rememberMe'); ?>
-		<?php echo $form->label($model,'rememberMe'); ?>
-		<?php echo $form->error($model,'rememberMe'); ?>
-	</div>
+	
 
 	<div class="row buttons">
-		<?php echo CHtml::submitButton('Login'); ?>
+		<?php echo CHtml::submitButton('Entrar'); ?>
 	</div>
 
 <?php $this->endWidget(); ?>
