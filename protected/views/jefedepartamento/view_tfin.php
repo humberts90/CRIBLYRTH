a800d51a125da5cfc6f57ff80814a17c7df986b7
josafatbecerra <josafat.becerra@gmail.com>
2014-07-26 22:00:15 -0430

    arreglo de algunos errores y formatos y cambio de la palabra culminada

--- protected/views/jefedepartamento/view_tfin.php
+++ protected/views/jefedepartamento/view_tfin.php
@@ -4,9 +4,9 @@
 	<tr>
 		<td width="300px"><?php echo CHtml::encode($data->Titulo); ?></td>
 		<td width="100px"><?php echo CHtml::encode($data->Fecha_Inscripcion); ?></td>
-		<td  width="100px"><?php echo CHtml::encode($data->Fecha_Aprobación); ?></td>
-		<td  width="100px"><?php echo CHtml::encode($data->Fecha_Defensa); ?></td>
-		<td  width="80px"><?php echo CHtml::link(CHtml::encode("Ver detalles"), array('comision/tesdeta', 'id'=>$data->id)); ?></td>
+		<td  width="110px"><?php echo CHtml::encode($data->Fecha_Aprobación); ?></td>
+		<td  width="110px"><?php echo CHtml::encode($data->Fecha_Defensa); ?></td>
+		<td  width="100px"><?php echo CHtml::link(CHtml::encode("Ver detalles"), array('comision/tesdeta', 'id'=>$data->id)); ?></td>
 		
 
 	</tr>
